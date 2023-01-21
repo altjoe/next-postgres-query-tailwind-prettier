0ef0d10d365f6c9a09d0fdd800dee09f12c21ad9
altjoe <meyer.joer5@gmail.com>
2023-01-21 15:21:12 -0500

    prettier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,13 @@
 module.exports = {
-  "stories": [
-    "../components/**/*.stories.js",
-    "../pages/**/*.stories.js"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-postcss"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+    stories: ["../components/**/*.stories.js", "../pages/**/*.stories.js"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/addon-postcss",
+    ],
+    framework: "@storybook/react",
+    core: {
+        builder: "@storybook/builder-webpack5",
+    },
+};
