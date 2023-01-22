2aa1d74bda7c7705dda72dd3d7174962bcb1cd3d
altjoe <meyer.joer5@gmail.com>
2023-01-21 20:59:26 -0500

    corrected api example

--- pages/api/example.js
+++ pages/api/example.js
@@ -4,8 +4,8 @@ export default async (req, res) => {
     client.connect();
 
     // console.log("create table if not exists");
-    const result = await client.query("select datname from db_schema");
-    
+    const result = await client.query("select datname from pg_database");
+
     await client.clean();
     await client.end();
 
