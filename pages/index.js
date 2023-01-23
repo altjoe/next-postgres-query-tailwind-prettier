6e9ea3044c10797cca88c16e3d4ca5abd18b1c37
JoeMac <meyer.joer5@gmail.com>
2023-01-23 14:22:32 -0500

    add callback example to template

--- pages/index.js
+++ pages/index.js
@@ -5,25 +5,31 @@ export default function Index(props) {
     const [triggerTable, setTriggerTable] = useState(false);
     // const useCreateTable = handleCreateTable('test', '(id, test)', triggerTable)
 
-    useEffect(() => {
-        fetch("api/create-table")
-            .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((err) => console.log(err));
-    }, []);
+    // useEffect(() => {
+    //     fetch("api/create-table")
+    //         .then((res) => res.json())
+    //         .then((data) => console.log(data))
+    //         .catch((err) => console.log(err));
+    // }, []);
 
-    useEffect(() => {
-        fetch("api/insert-row")
-            .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((err) => console.log(err))
-            .finally(
-                fetch("api/get-table")
-                    .then((res) => res.json())
-                    .then((data) => console.log(data))
-                    .catch((err) => console.log(err))
-            );
-    }, []);
+    // useEffect(() => {
+    //     fetch("api/insert-row")
+    //         .then((res) => res.json())
+    //         .then((data) => console.log(data))
+    //         .catch((err) => console.log(err))
+    //         .finally(
+    //             fetch("api/get-table")
+    //                 .then((res) => res.json())
+    //                 .then((data) => console.log(data))
+    //                 .catch((err) => console.log(err))
+    //         );
+    // }, []);
 
-    return <div className={``}></div>;
+    return (
+        <div className={`flex h-screen flex-col bg-gradient-to-tr from-slate-700 to-slate-500`}>
+            <div className={`flex-1`}></div>
+            <div className={`text-center text-3xl text-white`}>Working on this...</div>
+            <div className={`flex-1`}></div>
+        </div>
+    );
 }
